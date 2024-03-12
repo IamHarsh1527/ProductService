60205d29d4b049358e6abaf9bbd0f1c5cfc556ac
Harsh <harsh.monti15@gmail.com>
2024-03-12 18:04:01 +0530

    database add kiya

--- src/main/java/com/scaler/Ecommerce/ProductService/models/Category.java
+++ src/main/java/com/scaler/Ecommerce/ProductService/models/Category.java
@@ -1,9 +1,13 @@
 package com.scaler.Ecommerce.ProductService.models;
 
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Getter
 @Setter
+@Entity
 public class Category extends Base{
+    private String name;
 }
--- src/main/java/com/scaler/Ecommerce/ProductService/models/Product.java
+++ src/main/java/com/scaler/Ecommerce/ProductService/models/Product.java
@@ -1,14 +1,25 @@
 package com.scaler.Ecommerce.ProductService.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Getter
 @Setter
+@Entity
 public class Product extends Base{
+
     private String title ;
+
+    @ManyToOne
     private Category category;
-    private String desc;
+
+    private String descri;
+
     private double price;
+
     private String image;
 }
+
